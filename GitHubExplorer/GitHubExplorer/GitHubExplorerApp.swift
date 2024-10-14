b13fc39bc2c1766ef0de49c12cd473d2edad5f56
Abhishek Verma <abhishek.rmv@gmail.com>
2024-10-14 18:31:35 +0530

    Initial project Setup

---
+++ GitHubExplorer/GitHubExplorer/GitHubExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExplorerApp.swift
+//  GitHubExplorer
+//
+//  Created by Abhishek on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
